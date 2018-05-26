95c9493ca86544c57e43f15250dcd8977a71bf30
Alexander Molnar <mathic@gmail.com>
2018-05-26 14:41:22 -0400

    Adjusted margins

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
   }
 
   function setSizes() {
-    margin = { top: 20, right: 40, bottom: 50, left: 50 };
+    margin = { top: 10, right: 40, bottom: 50, left: 50 };
     height = 450 - margin.top - margin.bottom;
     width = 850 - margin.left - margin.right;
     x = d3.scaleTime().range([0, width]);
@@ -45,7 +45,7 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
 
   function drawGraph() {
     svg.append('g')
-      .attr('transform', `translate(0, ${height})`)
+      .attr('transform', `translate(${margin.left}, ${margin.top+height})`)
       .call(d3.axisBottom(x).tickFormat(d3.timeFormat('%M:%S')));
 
     svg.append('text')
@@ -55,6 +55,7 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
       .text('Ascent Time for the 13.8km climb');
 
     svg.append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
       .call(d3.axisLeft(y));
 
     svg.append('text')
@@ -71,6 +72,7 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
       .attr('cx', (d) => x(new Date(1969, 0, 0, 0, 0, d.Seconds)))
       .attr('cy', (d) => y(d.Place))
       .attr('fill', (d) => !!d.Doping ? '#f44' : '#333')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
       .on('mouseover', (d) => mouseover(d))
       .on('mouseout', mouseout);
 
@@ -105,7 +107,7 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
       .style('font-size', '12px')
       .attr('x', (d) => x(new Date(1969, 0, 0, 0, 0, d.Seconds)))
       .attr('y', (d) => y(d.Place))
-      .attr('transform', 'translate(15,5)');
+      .attr('transform', `translate(${15+margin.left},${5+margin.top})`);
   }
 
   function setLegend() {
